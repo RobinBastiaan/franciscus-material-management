1b665080d4ddf6baae5ebc2e068787b7b826c402
RobinBastiaan <robinbastiaan@gmail.com>
2021-09-05 19:48:26 +0200

    :bug: Fix db url format

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 require dirname(__DIR__).'/vendor/autoload.php'; // DEV
 //require __DIR__.'/../symfony/vendor/autoload.php'; // PROD
 //$_SERVER['APP_ENV']='prod';
-//$_SERVER['DATABASE_URL']='mysql://robin:pwd@witherhosting.com:2083/cweufdea_franciscus_material_management';
+//$_SERVER['DATABASE_URL']='mysql://cweufdea_robin:pwd@localhost:3306/cweufdea_franciscus_material_management';
 
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
