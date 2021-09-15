076320e31fb389bcc12b2164304f6b6c1ebd40e5
RobinBastiaan <robinbastiaan@gmail.com>
2021-09-15 23:58:52 +0200

    :sparkles: Add custom action to hand in loan

--- src/Controller/Admin/LoanCrudController.php
+++ src/Controller/Admin/LoanCrudController.php
@@ -4,8 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Loan;
 use App\Entity\Material;
+use DateTime;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
@@ -14,9 +18,17 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\NullFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 class LoanCrudController extends AbstractCrudController
 {
+    private AdminUrlGenerator $adminUrlGenerator;
+
+    public function __construct(AdminUrlGenerator $adminUrlGenerator)
+    {
+        $this->adminUrlGenerator = $adminUrlGenerator;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Loan::class;
@@ -57,5 +69,32 @@ class LoanCrudController extends AbstractCrudController
                 ->setChoices(array_merge(['Nog niet ingeleverd' => null], array_combine(Material::STATES, Material::STATES)))->setLabel('Staat'))
             ->add(EntityFilter::new('createdBy')->setLabel('Toegevoegd door'))
             ->add(NullFilter::new('deletedAt')->setLabel('Verwijderd')->setChoiceLabels('Nee', 'Ja'));
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $handInLoan = Action::new('handInLoan', 'Inleveren')
+            ->linkToCrudAction('handInLoan');
+
+        return $actions
+            ->add(Crud::PAGE_INDEX, $handInLoan);
+    }
+
+    public function handInLoan(AdminContext $context)
+    {
+        /** @var Loan $loan */
+        $loan = $context->getEntity()->getInstance();
+        $loan->setReturnedState('Goed');
+        $loan->setDateReturned(new DateTime());
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($loan);
+        $manager->flush();
+
+        $url = $this->adminUrlGenerator
+            ->setController(LoanCrudController::class)
+            ->setAction(Action::INDEX)
+            ->generateUrl();
+
+        return $this->redirect($url);
     }
 }
